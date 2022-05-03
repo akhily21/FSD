17da9cdc851aad0c8361e1366c3acd2377e65ab5
akhily21 <104872907+akhily21@users.noreply.github.com>
2022-05-03 19:42:53 -0400

    Add files via upload

--- classExercise1.py
+++ classExercise1.py
@@ -1,6 +1,8 @@
-input1 = input('enter first value',)
-input2 = input('enter second value',)
+input1 = int(input('enter first value',))
+input2 = int(input('enter second value',))
 
-print('product= ', a*b)
-print('sum= ' a+b)
-print('difference= 'a-b)
+print('product= ', input1*input2)
+print('sum= ', input1+input2)
+print('difference= ',input1-input2)
+print('reminder= ', input1%input2)
+
